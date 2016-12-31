07c988dbf26144832ab5e2b987bb10514e730ff0
Kurt Stephens <ks.github@kurtstephens.com>
2016-12-31 02:05:50 -0600

    Clean up.

--- lib/composite_type.rb
+++ lib/composite_type.rb
@@ -7,13 +7,3 @@ require 'composite_type/module_extensions'
 ::Module.send(:include, CompositeType::ModuleExtensions)
 require 'composite_type/numeric'
 
-# Objects that can do IO.
-#
-# Note: IO and StringIO do not share a common ancestor Module
-# that distingushes them as being capable of "IO".
-# So we create one here -- devdriven.com 2013/11/14
-require 'stringio'
-module IOable
-  ::IO.send(:include, self)
-  ::StringIO.send(:include, self)
-end
